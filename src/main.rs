f199160c24ea1c6664b27106c05ceb6417a523db
Drew Bubar <andrewpbubar@gmail.com>
2022-04-18 18:12:42 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+
+use bevy::{prelude::*, render::camera::ScalingMode};
+
+pub const CLEAR: Color = Color::rgb(0.1,0.1,0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+fn main(){
+
+    let height = 900.0;
+
+    App::new()
+
+    .insert_resource(ClearColor(CLEAR))
+    .insert_resource(WindowDescriptor {
+        width: height * RESOLUTION,
+        height: height,
+        title: "Bevy Test".to_string(),
+        vsync: true,
+        resizable: false,
+        ..Default::default()
+    })
+
+    //start systems
+    .add_startup_system(spawn_camera)
+    .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
+
+    .add_plugins(DefaultPlugins)
+    .run();
+}
+
+fn spawn_camera(mut commands: Commands){
+
+    let mut camera = OrthographicCameraBundle::new_2d();
+
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+
+    camera.orthographic_projection.right = 1.0 * RESOLUTION;
+    camera.orthographic_projection.left = -1.0 * RESOLUTION;
+
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
+}
+
+struct AsciiSheet(Handle<TextureAtlas>);
+
+fn load_ascii(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    texture_atlases: Res<Assets<TextureAtlas>>
+) {
+    let image = assets.load("Ascii.png");
+    
+    let atlas = TextureAtlas::from_grid_with_padding(
+        image,
+        Vec2::splat(9.0),
+        16,
+        16,
+        Vec2::splat(2.0)
+    );
+
+    let atlas_handle: Handle<TextureAtlas> 
+        = texture_atlases.add(atlas);
+
+    commands.insert_resource(AsciiSheet(atlas_handle));
+}
