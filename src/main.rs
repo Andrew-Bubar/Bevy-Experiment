ec814ac30f3e43d5880c34912b278c8b63910181
Andrew Bubar <Andrewpbubar@gmail.com>
2022-04-19 15:17:58 -0400

    Fixed movement and cleaned up ascii sprite loading. Working on a tile map system now.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
 
 mod player;
+mod debug;
+mod ascii;
+
+use debug::DebugPlugin;
 use player::PlayerPlugin;
+use ascii::AsciiPlugin;
 
 pub const CLEAR: Color = Color::rgb(0.1,0.1,0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
 
 fn main(){
 
@@ -26,10 +32,14 @@ fn main(){
 
     //start systems
     .add_startup_system(spawn_camera)
-    .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
 
     .add_plugins(DefaultPlugins)
+
+    //my plugins
     .add_plugin(PlayerPlugin)
+    .add_plugin(DebugPlugin)
+    .add_plugin(AsciiPlugin)
+
     .run();
 }
 
@@ -48,24 +58,3 @@ fn spawn_camera(mut commands: Commands){
     commands.spawn_bundle(camera);
 }
 
-struct AsciiSheet(Handle<TextureAtlas>);
-
-fn load_ascii(
-    mut commands: Commands,
-    assets: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>
-) {
-    let image = assets.load("Ascii.png");
-    
-    let atlas = TextureAtlas::from_grid_with_padding(
-        image,
-        Vec2::splat(9.0),
-        16,
-        16,
-        Vec2::splat(2.0)
-    );
-
-    let atlas_handle: Handle<TextureAtlas> = texture_atlases.add(atlas);
-
-    commands.insert_resource(AsciiSheet(atlas_handle));
-}
--- src/player.rs
+++ src/player.rs
@@ -1,18 +1,22 @@
 
 use bevy::prelude::*;
-use crate::AsciiSheet;
+use bevy_inspector_egui::Inspectable;
+
+use crate::{ascii::{AsciiPlugin, AsciiSheet, spawn_ascii_sprite}, TILE_SIZE};
 
 pub struct PlayerPlugin;
 
 //making the player more special
-#[derive(Component)]
-pub struct Player;
+#[derive(Component, Inspectable)]
+pub struct Player{
+    speed: f32,
+}
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App){
         app
         .add_startup_system(spawn_player)
-        .add_startup_system(player_movement);
+        .add_system(player_movement);
     }
 }
 
@@ -20,64 +24,59 @@ impl Plugin for PlayerPlugin {
 fn player_movement(
     mut player_query: Query<(&Player, &mut Transform)>,
     keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
 ){
     //making the player be both a sprite and transform
     let(_player, mut transform) = player_query.single_mut();
 
     //inputs
-    if keyboard.pressed(KeyCode::W){
-        transform.translation.y += 0.5;
+    if keyboard.pressed(KeyCode::W) | keyboard.pressed(KeyCode::Up){
+        transform.translation.y += _player.speed * TILE_SIZE * time.delta_seconds();
     }
-    if keyboard.pressed(KeyCode::S){
-        transform.translation.y -= 0.5;
+    if keyboard.pressed(KeyCode::S) | keyboard.pressed(KeyCode::Down) {
+        transform.translation.y -= _player.speed * TILE_SIZE * time.delta_seconds();
     }
-    if keyboard.pressed(KeyCode::A){
-        transform.translation.x -= 0.5;
+    if keyboard.pressed(KeyCode::A) | keyboard.pressed(KeyCode::Left) {
+        transform.translation.x -= _player.speed * TILE_SIZE * time.delta_seconds();
     }
-    if keyboard.pressed(KeyCode::D){
-        transform.translation.x += 0.5;
+    if keyboard.pressed(KeyCode::D) | keyboard.pressed(KeyCode::Right) {
+        transform.translation.x += _player.speed * TILE_SIZE * time.delta_seconds();
     }
 }
 
 fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>){
 
-    //player sprite stuff
-    let mut sprite = TextureAtlasSprite::new(1);
-    sprite.color = Color::rgb(0.3,0.3,0.9);
-    sprite.custom_size = Some(Vec2::splat(1.0));
+    //player ascii entity
+    let player = spawn_ascii_sprite(
+        &mut commands,
+        &ascii,
+        1,
+        Color::rgb(0.3,0.3,0.9),
+        Vec3::new(0.0,0.0,900.0),
+    );
 
-    //Background player sprite stuff
-    let mut background_sprite = TextureAtlasSprite::new(0);
-    background_sprite.color = Color::rgb(0.5,0.5,0.5);
-    background_sprite.custom_size = Some(Vec2::splat(1.0));
-     
-    //sawining in the player sprite
-    let player = commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: sprite,
-            texture_atlas: ascii.0.clone(), 
-            transform: Transform {
-                translation: Vec3::new(0.0,0.0,900.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
+    //commands for player spawn
+    commands
+        .entity(player)
         .insert(Name::new("Player"))
-        .insert(Player)
+        .insert(Player{ speed: 3.0})
         .id();
 
-    let background = commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: background_sprite,
-            texture_atlas: ascii.0.clone(), 
-            transform: Transform {
-                translation: Vec3::new(0.0,0.0,-1.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        }).insert(Name::new("Background"))
-        .id();
+        //player ascii entity
+        let background = spawn_ascii_sprite(
+            &mut commands,
+            &ascii,
+            0,
+            Color::rgb(0.5,0.5,0.5),
+            Vec3::new(0.0,0.0,-1.0),
+        );
+    
+        //commands for player spawn
+        commands
+            .entity(background)
+            .insert(Name::new("Background"))
+            .id();
 
-    //formating export
+    //making the background a child of the player
     commands.entity(player).push_children(&[background]);
 }
